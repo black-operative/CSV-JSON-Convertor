43e940eeac5cebfb707ae5092d37a55986feaea1
cheeky-bastard-04 <rudrakshm07@gmail.com>
2024-03-23 20:38:57 +0530

    Added include guard
    Added include guard to check for compiler compatibility

--- Header-Files/Err_Codes.h
+++ Header-Files/Err_Codes.h
@@ -1,3 +1,7 @@
+#pragma once
+#ifndef ERR_CODES
+#define ERR_CODES
+
 #include <string>
 
 std::string no_CSV_file  = "Error : No CSV file found \n";
@@ -7,3 +11,5 @@ std::string no_file_reader_init = "Error : Cannot open file for reading \n";
 std::string no_file_writer_init = "Error : Cannot open file for writing \n";
 
 std::string no_CSV_attributes = "Error : No Attributes found in CSV file \n";
+
+#endif
--- Header-Files/Header.h
+++ Header-Files/Header.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
 
 #include <iostream>
 #include <fstream>
@@ -19,3 +21,5 @@ enum useMode {
     CSV_TO_JSON,
     JSON_TO_CSV
 };
+
+#endif
